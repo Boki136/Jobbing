bea5c05098d53e4c5b3c02871773848d1dd31e2b
Boki136 <boris.gersic1@gmail.com>
2021-04-10 13:56:47 +0000

    refactor[job-expand]: tidy the code to achieve same funcionality with less code. Implement styling to various elements.

--- static/js/main.js
+++ static/js/main.js
@@ -51,30 +51,28 @@ $(".job-description").each(function (i) {
 // Show Only Selected Job on click
 let count = 0;
 $(".see-full-job").click(function () {
+  let job_expand = $(".job-expand");
+  job_expand.css("visibility", "visible");
   //check the count of click to avoid element duplication
   if (count == 0) {
     let job_title = $(this).parent().find("h1").text();
     let job_location = $(this).parent().find("h2").text();
     let job_salary = $(this).parent().find("h3").text();
     let job_description = $(this).parent().find("p").text();
-
+    let posted_date = $(this).parent().find("h4").text();
     // capture job-listing-box element and append to job-expand
 
-    $(this)
-      .parent()
-      .clone()
-      .html(
-        `<h1 class="job-title">${job_title}</h1>
-              <h2 class="contract-type-location">${job_location}</h2>
-              <h3 class="job-salary">${job_salary}</h3>
-              <hr>
-              <form action="{{url_for('find_job')}}" method="POST">
-              <button class="save_job">Save a Job</button>
-            </form>
-              <p class="job-description">${job_description}</p>
-              `
-      )
-      .appendTo($(".job-expand"));
+    $(this).parent().parent().parent().find(job_expand)
+      .append(`<h1 class="job-title">${job_title}</h1>
+      <h2 class="contract-type-location">${job_location}</h2>
+      <h3 class="job-salary">${job_salary}</h3>
+      <hr>
+      <form class="save-job_form" action="{{url_for('find_job')}}" method="POST">
+      <button class="save_job">Save a Job</button>
+    </form>
+      <p>${job_description}</p>
+      <h4 class="post-date">${posted_date}</h4>
+      `);
 
     count++;
 
@@ -84,21 +82,18 @@ $(".see-full-job").click(function () {
     let job_location = $(this).parent().find("h2").text();
     let job_salary = $(this).parent().find("h3").text();
     let job_description = $(this).parent().find("p").text();
+    let posted_date = $(this).parent().find("h4").text();
     $(".job-expand").children().remove();
-    $(this)
-      .parent()
-      .clone()
-      .html(
-        `<h1 class="job-title">${job_title}</h1>
-            <h2 class="contract-type-location">${job_location}</h2>
-            <h3 class="job-salary">${job_salary}</h3>
-            <hr>
-            <form action="{{url_for('find_job')}}" method="POST">
-            <button class="save_job">Save a Job</button>
-          </form>
-            <p class="job-description">${job_description}</p>
-            `
-      )
-      .appendTo($(".job-expand"));
+    $(this).parent().parent().parent().find(job_expand)
+      .append(`<h1 class="job-title">${job_title}</h1>
+    <h2 class="contract-type-location">${job_location}</h2>
+    <h3 class="job-salary">${job_salary}</h3>
+    <hr>
+    <form class="save-job_form" action="{{url_for('find_job')}}" method="POST">
+    <button class="save_job">Save a Job</button>
+  </form>
+    <p class="job-description">${job_description}</p>
+    <h4 class="post-date">${posted_date}</h4>
+    `);
   }
 });
